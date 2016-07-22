b48d75e7bf8e456e9c61ebec28ae628c1cd38791
Nagen <nagen.bnb@gmail.com>
2016-07-22 16:56:56 +0530

    initial commit

---
+++ www/app/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+
+
+
+(function() {
+  var cx = '000448580927239239790:xh4aaufifti';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
